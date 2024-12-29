49bcacaa88249464f1d72f2112c3e707db571863
Daniil Mikhailov <info@mdsina.ru>
2024-12-29 07:24:58 +0700

    Use custom sonarr package

--- apps/sonarr/ci/latest.sh
+++ apps/sonarr/ci/latest.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-channel=$1
-version=$(curl -sX GET "https://services.sonarr.tv/v1/download/${channel}?version=4.0" | jq --raw-output '.version' 2>/dev/null)
+#channel=$1
+version=$(curl -sX GET "https://api.github.com/repos/mdsina/Sonarr/releases" | jq --raw-output 'map(select(.prerelease)) | first | .tag_name' 2>/dev/null)
 version="${version#*v}"
 version="${version#*release-}"
 printf "%s" "${version}"
